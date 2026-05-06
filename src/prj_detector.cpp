589164bfc33852ddb24e9a66a92e86cf9f4e8791
wts <2470037708@qq.com>
2026-05-06 16:39:48 +0800

    天生毕业设计结束

--- src/prj_detector.cpp
+++ src/prj_detector.cpp
@@ -174,7 +174,7 @@ void prj_v8detector::camera()
 void prj_v8detector::camera_foldimages()
 {
     std::vector<cv::String> filenames;
-    cv::String folder = "/home/cvia/yifei/images_old3/*.png";
+    cv::String folder = "/home/cvia/yifei/images10/*.png";
     cv::glob(folder, filenames, false);
     std::sort(filenames.begin(), filenames.end());
     // std::sort(filenames.rbegin(), filenames.rend());
